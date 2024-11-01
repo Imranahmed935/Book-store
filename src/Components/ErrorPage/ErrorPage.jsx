74489947c0cf40b062b3229b696bdbcd240ece69
Imranahmed935 <imrantahir9918@gmail.com>
2024-11-01 04:34:51 -0700

    navbar added

---
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    
+    return (
+        <div>
+            <h1 className='text-4xl text-red-600'>Page not found</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Root from './Components/Root/Root.jsx'
+import Home from './Components/Home/Home.jsx'
+import ErrorPage from './Components/ErrorPage/ErrorPage.jsx'
 
 const router = createBrowserRouter([
   {
     path:'/',
-    element:<Root></Root>
+    element:<Root></Root>,
+    errorElement:<ErrorPage></ErrorPage>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      }
+    ]
   }
 ])
 
