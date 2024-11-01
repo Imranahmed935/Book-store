f0287a4dad36329ab57f3f55e0209a3b196c43b6
Imranahmed935 <imrantahir9918@gmail.com>
2024-11-01 09:28:57 -0700

    card loaded

--- src/Components/Book/Book.jsx
+++ src/Components/Book/Book.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 
 const Book = ({book}) => {
-    console.log(book)
+    const {bookName, author, image, tags, category, rating} = book;
     return (
-        <div>
-            
+        <div className="card bg-base-100 border p-2">
+        <figure className='bg-[#F3F3F3] p-2'>
+        <img
+            src={image}
+            className='h-56 w-44'
+            alt={bookName} />
+        </figure>
+        <div className="card-body">
+            <div className='flex gap-6'>
+             {
+                tags.map((tag, idx)=> <button className=" bg-[#F3F3F3] text-green-500" key={idx}>{tag}</button>)
+             }
+             
+            </div>
+        <h2 className="card-title text-xl font-bold">
+           {bookName}
+        </h2>
+        <p className='text-xl'>By: {author}</p>
+        <div className='border-t-2 border-dashed mt-2'></div>
+        <div className="card-actions justify-between mt-4">
+            <div className="badge badge-outline">{category}</div>
+            <div className="badge badge-outline">{rating}</div>
+        </div>
+        </div>
         </div>
     );
 };
--- src/Components/Books/Books.jsx
+++ src/Components/Books/Books.jsx
@@ -12,9 +12,11 @@ const Books = () => {
     return (
         <div className='mt-10'>
             <h1 className='text-center text-4xl font-bold'>Books : {books.length}</h1>
+               <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
                 {
                     books.map(book => <Book key={book.bookId} book={book}></Book>)
                 }
+               </div>
         </div>
     );
 };
